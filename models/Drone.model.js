3f480244c7b7633935083f8962d36bd4d444a30d
Bharath Kompally <kompallybharath@gmail.com>
2024-05-20 11:13:57 +0530

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,9 @@
 // Iteration #1
+const mangoose=require('mangoose')
+const{Schema,model}=mangoose
+const droneSchema=new Schema({
+    name:{type:String,required:true},
+    propellers: { type: Number, required: true },
+   maxSpeed: { type: Number, required: true}
+});
+module.export=model('Drone',droneSchema);
